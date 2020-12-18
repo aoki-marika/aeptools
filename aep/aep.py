4483b0c3ae8d49ea987c9f20b753eed954eab924
marika <marika@waifu.club>
2020-12-18 12:51:42 -0400

    Add binary layer decoding.

--- aep/aep.py
+++ aep/aep.py
@@ -1,4 +1,5 @@
-from typing import Sequence
+from enum import Enum
+from typing import Optional, Sequence
 
 class Texture:
     pass
@@ -11,14 +12,56 @@ class Project(object):
         self.textures = textures
         self.compositions = compositions
 
+        # ensure there are no invalid asset references
+        for composition in compositions:
+            for layer in composition.layers:
+                if isinstance(layer, TextureLayer):
+                    assert(any(t.name == layer.asset_name for t in self.textures))
+                elif isinstance(layer, CompositionLayer):
+                    assert(any(c.name == layer.asset_name for c in self.compositions))
+
 class Texture(object):
     def __init__(self, name: str, width: int, height: int) -> None:
         self.name = name
         self.width = width
         self.height = height
 
+class Layer:
+    pass
+
 class Composition(object):
-    def __init__(self, name: str, width: int, height: int) -> None:
+    def __init__(self, name: str, width: int, height: int, layers: Sequence[Layer]) -> None:
         self.name = name
         self.width = width
         self.height = height
+        self.layers = layers
+
+class BlendMode(Enum):
+    # https://github.com/aoki-marika/aeptools/wiki/Format-(x86-and-x64)#blend-mode
+
+    NORMAL = 0x2
+    ADDITIVE = 0x4
+    UNKNOWN = 0x5
+
+class Layer(object):
+    def __init__(self, name: str, blend_mode: BlendMode) -> None:
+        self.name = name
+        self.blend_mode = blend_mode
+
+    @property
+    def asset_name(self):
+        # https://github.com/aoki-marika/aeptools/wiki/Format-(x86-and-x64)#layer
+
+        if '-' in self.name:
+            return self.name.split('-')[1]
+        else:
+            return self.name
+
+class TextureLayer(Layer):
+    pass
+
+class CompositionLayer(Layer):
+    pass
+
+class ColourLayer(Layer):
+    pass
--- aep/bin.py
+++ aep/bin.py
@@ -1,7 +1,7 @@
 from io import BufferedReader, BufferedWriter
 from enum import Enum
 from typing import Union
-from .aep import Project, Texture, Composition
+from .aep import Project, Texture, Composition, BlendMode, Layer, TextureLayer, CompositionLayer, ColourLayer
 
 ENDIANNESS = 'little'
 
@@ -63,8 +63,17 @@ class BinaryReader(object):
         return self.read_pointer()
 
 class AssetType(Enum):
+    # https://github.com/aoki-marika/aeptools/wiki/Format-(x86-and-x64)#type
+
     TEXTURE = 0x0
     COMPOSITION = 0x1
+
+class LayerType(Enum):
+    # https://github.com/aoki-marika/aeptools/wiki/Format-(x86-and-x64)#contents-type
+
+    COMPOSITION = 0x4
+    COLOUR = 0x6
+    TEXTURE = 0x7
 
 class BinaryDecoder(object):
     def __init__(self, architecture: Architecture) -> None:
@@ -107,16 +116,67 @@ class BinaryDecoder(object):
             num_layers = reader.read_count()
             assert(size == 32)
 
-        # ensure the cursor is reset for array reading
-        reader.seek(pointer + size)
-
         if type == AssetType.TEXTURE:
             assert(num_layers == 0)
             assert(layers_pointer == 0x0)
-            return Texture(name, width, height)
+            asset = Texture(name, width, height)
         elif type == AssetType.COMPOSITION:
             assert(layers_pointer != 0x0)
-            return Composition(name, width, height)
+
+            layers = []
+            reader.seek(layers_pointer)
+            for _ in range(0, num_layers):
+                layers.append(self._decode_layer(reader))
+
+            asset = Composition(name, width, height, layers)
+
+        # ensure the cursor is reset for array reading
+        reader.seek(pointer + size)
+        return asset
+
+    def _decode_layer(self, reader: BinaryReader) -> Layer:
+        # https://github.com/aoki-marika/aeptools/wiki/Format-(x86-and-x64)#layer
+
+        pointer = reader.tell()
+
+        size = reader.read_u16()
+        type_blend_mode = reader.read_u8()
+        type = LayerType((type_blend_mode >> 4) & 0xf)
+        blend_mode = BlendMode(type_blend_mode & 0xf)
+
+        # padding
+        assert(reader.read_u8() == 0x0)
+        if self.architecture == Architecture.X64:
+            assert(reader.read_u32() == 0x0)
+
+        name = reader.read_string()
+        timeline_pointer = reader.read_pointer()
+        position_keyframes_pointer = reader.read_pointer()
+        anchor_point_keyframes_pointer = reader.read_pointer()
+        colour_keyframes_pointer = reader.read_pointer()
+        scale_keyframes_pointer = reader.read_pointer()
+        alpha_keyframes_pointer = reader.read_pointer()
+        unknown_keyframes_pointer = reader.read_pointer()
+        rotation_x_keyframes_pointer = reader.read_pointer()
+        rotation_y_keyframes_pointer = reader.read_pointer()
+        rotation_z_keyframes_pointer = reader.read_pointer()
+        size_keyframes_pointer = reader.read_pointer()
+        marker_keyframes_pointer = reader.read_pointer()
+
+        if self.architecture == Architecture.X86:
+            assert(size == 56)
+        elif self.architecture == Architecture.X64:
+            assert(size == 112)
+
+        if type == LayerType.COMPOSITION:
+            return CompositionLayer(name, blend_mode)
+        elif type == LayerType.COLOUR:
+            return ColourLayer(name, blend_mode)
+        elif type == LayerType.TEXTURE:
+            return TextureLayer(name, blend_mode)
+
+        # ensure the cursor is reset for array reading
+        reader.seek(pointer + size)
 
 class BinaryEncoder(object):
     def __init__(self, architecture: Architecture) -> None:
